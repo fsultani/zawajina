ce545ef51eb38ac2a875e1325b7a32b0995dc7e7
Farid Sultani <Farid@farids-mbp.attlocal.net>
2017-05-18 23:11:15 -0700

    Working with conditionals.

--- routes/index.js
+++ routes/index.js
@@ -57,26 +57,54 @@ router.get('/register', function(req, res, next) {
   res.render('register')
 });
 
+// helpers: {
+//   is: function(a, b, options) {
+//     if (a == b) {
+//       return options.fn(this);
+//     }
+//   }
+// }
+
 router.get('/user/:username', ensureAuthenticated, function(req, res, next) {
 	console.log("User that's logged in:", req.user)
 	User.findOne({ username: req.params.username }, function(err, user) {
 		console.log("User being viewed:", user)
 		if (err) throw err
 		else {
-			res.render('profile', {
-				user: user,
-				currentUser: req.user,
-				helpers: {
-            is: function(a, b, options) {
-              if (a == b) {
-                return options.fn(this);
-              }
-            }
-          }
-			})
+			if (req.user.username == user.username) {
+				data = {
+					logged_in: req.user,
+					current_user: req.user
+				}
+			} else {
+				data = {
+					logged_in: user,
+					other_user: user
+				}
+			}
+			res.render('profile', data)
 		}
 	})
 });
+
+// router.get('/user/:username', ensureAuthenticated, function(req, res, next) {
+// 	console.log("User that's logged in:", req.user)
+// 	User.findOne({ username: req.params.username }, function(err, user) {
+// 		console.log("User being viewed:", user)
+// 		if (err) throw err
+// 		else {
+// 			if (req.user.username == user.username) {
+// 				res.render('profile', {
+// 					user: req.user
+// 				})
+// 			} else {
+// 				res.render('profile_user', {
+// 					user: user
+// 				})
+// 			}
+// 		}
+// 	})
+// });
 
 router.get('/login', function(req, res, next) {
   res.render('login')
