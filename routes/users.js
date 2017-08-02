ed9b8c7c3ba75e828d3c78c03a79385439105d43
Farid Sultani <Farid@farids-mbp.attlocal.net>
2017-08-02 01:17:08 -0700

    Convert tabs to spaces in routes/users.js.

--- routes/users.js
+++ routes/users.js
@@ -7,48 +7,48 @@ var Message = require('../models/message')
 var Conversation = require('../models/conversation')
 
 function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		res.redirect('/');
-	}
+  if(req.isAuthenticated()){
+    return next();
+  } else {
+    res.redirect('/');
+  }
 }
 
 router.get('/profile', ensureAuthenticated, (req, res, next) => {
-	res.redirect('/users/' + req.user.username)
+  res.redirect('/users/' + req.user.username)
 })
 
 // Get the logged in user's profile
 router.get('/member', ensureAuthenticated, (req, res, next) => {
-	Conversation.find({ users: req.user._id }, (err, conversations) => {
-		User.findOne({ _id: req.user._id }, (err, user) => {
-			var conversations_count = 0
-			conversations.map((conversation) => {
-				if (req.user._id.toString() === conversation.sent_to_user_id && conversation.unread) {
-					conversations_count += 1
-				}
-			})
-			res.render('user_profile', {
-				conversations_count: conversations_count,
-				conversations: conversations,
-				helpers: {
-		      if_eq: function(a, b, options) {
-		        if (a == b) {
-		          return options.fn(this);
-		        } else {
-		        	return options.inverse(this)
-		        }
-		      }
-		    }
-		  })
-		})
-	})
+  Conversation.find({ users: req.user._id }, (err, conversations) => {
+    User.findOne({ _id: req.user._id }, (err, user) => {
+      var conversations_count = 0
+      conversations.map((conversation) => {
+        if (req.user._id.toString() === conversation.sent_to_user_id && conversation.unread) {
+          conversations_count += 1
+        }
+      })
+      res.render('user_profile', {
+        conversations_count: conversations_count,
+        conversations: conversations,
+        helpers: {
+          if_eq: function(a, b, options) {
+            if (a == b) {
+              return options.fn(this);
+            } else {
+              return options.inverse(this)
+            }
+          }
+        }
+      })
+    })
+  })
 })
 
 router.get('/:username', ensureAuthenticated, (req, res, next) => {
-	User.findOne({ username: req.params.username }, (err, member) => {
-		res.render('member_profile', { member: member })
-	})
+  User.findOne({ username: req.params.username }, (err, member) => {
+    res.render('member_profile', { member: member })
+  })
 });
 
 module.exports = router;
