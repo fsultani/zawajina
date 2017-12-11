706c8b8eadb0f88e3a4cffdf4f7ebbd24b463f41
Farid Sultani <Farid@farids-mbp.attlocal.net>
2017-12-11 08:35:47 -0800

    Experiment with callbacks in the server

--- models/conversation.js
+++ models/conversation.js
@@ -4,10 +4,9 @@ var Conversation = mongoose.Schema({
   created_at: Date,
   updated_at: Date,
   created_by: String,
-  created_by_username: String,
+  created_by_user_first_name: String,
   created_by_user_id: String,
-  sent_to: String,
-  sent_to_username: String,
+  sent_to_user_first_name: String,
   sent_to_user_id: String,
   unread: Boolean,
   users: [
--- routes/messages.js
+++ routes/messages.js
@@ -27,46 +27,86 @@ router.get('/api/all-messages', (req, res, next) => {
 // Send a new message to another user
 ****************************************************************************************************/
 
-router.post('/:user_id', (req, res, next) => {
-  // User.findById({ _id: req.params.user_id }, (err, user) => {
-  //   Conversation.find({ $and: [{ users: req.user._id }, { users: user._id }] }, (err, conversation) => {
-  //     if (conversation.length === 0) {
-  //       Conversation.create({
-  //         created_at: Date.now(),
-  //         updated_at: Date.now(),
-  //         created_by_user_name: req.user.first_name,
-  //         created_by_user_id: req.user._id,
-  //         sent_to_user_name: user.first_name,
-  //         sent_to_user_id: user._id,
-  //         unread: true
-  //       }, (err, conversation) => {
-  //         if (err) {
-  //           console.log(err)
-  //         } else {
-  //           Message.create({
-  //             message: req.body.message,
-  //             from: req.user.first_name,
-  //             to: user.first_name,
-  //             from_user_id: req.user._id,
-  //             to_user_id: user._id,
-  //             created_at: Date.now(),
-  //             unread: true
-  //           }, (err, message) => {
-  //             if (err) {
-  //               console.log(err)
-  //             } else {
-  //               conversation.users.push(req.user, user)
-  //               message.conversations.push(conversation)
-  //               conversation.save()
-  //               message.save()
-  //               res.redirect('/conversations/' + conversation._id)
-  //             }
-  //           })
-  //         }
-  //       })
-  //     } else {
-  //       res.redirect('/conversations/' + conversation[0]._id)
-  //     }
+router.post('/api/new-message', (req, res, next) => {
+  const token = req.headers['user-cookie']
+  const decodedUser = jwt.decode(token, JWT_SECRET)
+
+  const contactedUser = (person, cb) => {
+    User.findOne({ _id: person }).exec((err, user) => {
+      console.log("user\n", user)
+      cb(user)
+    })
+  }
+
+  // const member = (member, cb) => {
+  //   User.find({ username: member }).exec((err, member) => {
+  //     cb(null, member[0])
+  //   })
+  // }
+
+  contactedUser(req.body.userId, (err, user) => {
+    console.log("user\n", user)
+  })
+
+  // member(decodedUser.username, (err, member) => { return member._id })
+
+  Conversation.find({
+    $and:
+      [
+        { users: contactedUser() },
+        { users: member() }
+      ]
+    }, (err, conversation) => {
+    console.log("conversation\n", conversation)
+  })
+
+  // User.findOne({ _id: req.body.userId }, (err, user) => {
+  //   console.log("user\n", user)
+  //   User.findOne({ username: decodedUser.username }, (err, member) => {
+  //     console.log("member\n", member)
+  //     Conversation.find({ $and: [{ users: user._id }, { users: member._id }] }, (err, conversation) => {
+  //       if (conversation.length === 0) {
+  //         console.log("conversation\n", conversation)
+  //         // Conversation.create({
+  //         //   created_at: Date.now(),
+  //         //   updated_at: Date.now(),
+  //         //   created_by_user_first_name: member.first_name,
+  //         //   created_by_user_id: member._id,
+  //         //   sent_to_user_first_name: user.first_name,
+  //         //   sent_to_user_id: user._id,
+  //         //   unread: true
+  //         // }, (err, conversation) => {
+  //         //   if (err) {
+  //         //     console.log(err)
+  //         //   } else {
+  //         //     console.log("conversation\n", conversation)
+  //         //     Message.create({
+  //         //       message: req.body.message,
+  //         //       from: member.first_name,
+  //         //       to: user.first_name,
+  //         //       from_user_id: member._id,
+  //         //       to_user_id: user._id,
+  //         //       created_at: Date.now(),
+  //         //       unread: true
+  //         //     }, (err, message) => {
+  //         //       if (err) {
+  //         //         console.log(err)
+  //         //       } else {
+  //         //         console.log("message\n", message)
+  //         //         conversation.users.push(member, user)
+  //         //         message.conversations.push(conversation)
+  //         //         conversation.save()
+  //         //         message.save()
+  //         //         // res.redirect('/conversations/' + conversation._id)
+  //         //       }
+  //         //     })
+  //         //   }
+  //         // })
+  //       } else {
+  //         console.log("conversation > 1\n", conversation)
+  //         res.redirect('/conversations/' + conversation[0]._id)
+  //       }
+  //     })
   //   })
   // })
 })
