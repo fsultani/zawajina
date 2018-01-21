a68f577b293af572301976718a22c1b05d068ba1
fsultani <farid@farids-mbp.home>
2018-01-21 13:22:56 -0800

    Allow replies to be sent

--- public/javascripts/views/conversation.js
+++ public/javascripts/views/conversation.js
@@ -1,13 +1,19 @@
 const conversationId = window.location.pathname.split('/')[2]
 
+let htmlOutput = beginLayout + authenticated + endLayout
+
 const sendReply = () => {
-  const message = document.getElementById("replyMessage").value
+  const reply = document.getElementById('replyMessage').value
   axios.post('/messages/api/reply', {
+    memberFirstName: Cookies.get('first_name'),
+    memberId: Cookies.get('id'),
     conversationId: conversationId,
-    message: message
+    reply: reply
   })
   .then(function(res) {
-    window.location.pathname = `/conversation/${res.data.conversation._id}`
+    if (res.status === 201) {
+      window.location.reload(true);      
+    }
   })
 }
 
@@ -20,7 +26,7 @@ window.addEventListener('load', () => {
       let messageList = `<center>`;
       messages.map((message) => {
         messageList += `
-          <div class="col-md-6 col-md-offset-3">
+          <div class="col-md-6 col-md-offset-3" id="messageList">
             <div class="well">
               <div align="left">`
 
@@ -42,19 +48,19 @@ window.addEventListener('load', () => {
 
       const reply = `
         <div id="contactForm">
-            <div class="form-group col-md-6 col-md-offset-3">
-              <center>
-                <textarea class="form-control" id="replyMessage" rows="5" cols="10"></textarea>
-                <br />
-                <button class="btn btn-primary" onclick="sendReply()">Send</button>
-              </center>
-            </div>
+          <div class="form-group col-md-6 col-md-offset-3">
+            <center>
+              <textarea class="form-control" id="replyMessage" rows="5" cols="10"></textarea>
+              <br />
+              <button class="btn btn-primary" onclick="sendReply()">Send</button>
+            </center>
           </div>
-        `;
+        </div>
+      `;
 
       messageList += `</center>`;
 
-      const htmlOutput = beginLayout + authenticated + endLayout + messageList + reply
+      htmlOutput += messageList + reply
       document.getElementById('my-app').innerHTML = htmlOutput;
     })
   } else if (url[1] === 'users' && url[3] === 'about') {
