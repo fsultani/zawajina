cbcbf509cea2a5a7a98ae3a6f6409243d94a100f
fsultani <farid@Personal.local>
2018-03-26 03:11:07 -0700

    Update the registration page

--- public/javascripts/views/register.js
+++ public/javascripts/views/register.js
@@ -1,54 +1,89 @@
+const day = () => {
+  const dayOptions = []
+  const res = [...Array(32)].map((_, i) => dayOptions.push(`<option>${i+1}</option>`))
+  return dayOptions
+}
+
+const year = () => {
+  let yearOptions = []
+  const res = [...Array(61)].map((_, i) => yearOptions.push(`<option>${1940+i}</option>`))
+  return yearOptions.reverse()
+}
+
 register = `
-  <form method="post" action="/register">
-    <div class="row">
-      <div class="form-group col-md-4 col-md-offset-2">
+  <div class="registrationContainer centerContainer">
+    <form method="post" action="/register">
+      <div class="form-group">
         <input type="text" class="form-control" placeholder="First Name" name="first_name">
       </div>
-      <div class="form-group col-md-4">
-        <input type="text" class="form-control" placeholder="Last Name" name="last_name">
-      </div>
-    </div>
 
-    <div class="row">
-      <div class="form-group col-md-4 col-md-offset-2">
-        <input type="text" class="form-control" placeholder="Username" name="username">
-      </div>
-       <div class="form-group col-md-4">
+       <div class="form-group">
         <input type="email" class="form-control" placeholder="Email" name="email">
       </div>
-    </div>
 
-    <div class="row">
-      <div class="form-group col-md-4 col-md-offset-2">
+      <div class="form-group">
         <input type="password" class="form-control" placeholder="Password" name="password">
       </div>
-      <div class="form-group col-md-4">
-        <input type="password" class="form-control" placeholder="Confirm Password" name="confirm_password">
-      </div>
-    </div>
 
-    <div class="row">
-      <div class="form-group col-md-4 col-md-offset-4">
-        <center>
-          <div>
-            <label for="male">Male</label>
-            <input id="male" type="radio" name="gender" value="male">
-          </div>
+      <div style="display: flex">
+        <div style="width: 25%">
+          I'm a
+        </div>
+        <div class="form-group" style="width: 50%">
+          <center>
+            <div class="gender-male">
+              <label for="male">Male</label>
+              <input id="male" type="radio" name="gender" value="male">
+            </div>
 
-          <div>
-            <label for="female">Female</label>
-            <input id="female" type="radio" name="gender" value="female">
-          </div>
-        </center>
+            <div class="gender-female">
+              <label for="female">Female</label>
+              <input id="female" type="radio" name="gender" value="female">
+            </div>
+          </center>
+        </div>
       </div>
-    </div>
 
-    <div class="row">
-      <div class="col-md-4 col-md-offset-4 text-center">
-        <button type="submit" class="btn btn-success">Sign Up</button>
+      <div>
+        <div>Date of Birth</div>
+        <div class="form-group col-md-4" style="padding-left: 0">
+          <select id="selectMonth" class="form-control">
+            <option>Month</option>
+            <option>January</option>
+            <option>February</option>
+            <option>March</option>
+            <option>April</option>
+            <option>May</option>
+            <option>June</option>
+            <option>July</option>
+            <option>August</option>
+            <option>September</option>
+            <option>October</option>
+            <option>November</option>
+            <option>December</option>
+          </select>
+        </div>
+        <div class="form-group col-md-4">
+          <select id="selectDay" class="form-control">
+            <option>Day</option>
+            ${day()}
+          </select>
+        </div>
+        <div class="form-group col-md-4">
+          <select id="selectYear" class="form-control">
+            <option>Year</option>
+            ${year()}
+          </select>
+        </div>
       </div>
-    </div>
-  </form>
+
+      <div class="row">
+        <div class="col-md-4 col-md-offset-4 text-center">
+          <button type="submit" class="btn btn-success">Sign Up</button>
+        </div>
+      </div>
+    </form>
+  </div>
 `;
 
 const registrationPage = layout + register
