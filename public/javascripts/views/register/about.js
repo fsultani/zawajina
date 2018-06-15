0d7bbea12e6c783129e2a351c2ddfc5563a3aae1
fsultani <farid@Personal.local>
2018-06-15 02:43:11 -0700

    Move register to new folder

---
+++ public/javascripts/views/register/about.js
@@ -0,0 +1,124 @@
+almostDone = `<center><h2>Sweet!  Almost done</h2></center>`
+
+titleStyle = {
+  'font-size': '16px',
+  'color': '#727272',
+  'text-align': 'center',
+}
+
+title = `
+  <div id="copy">
+    <p>
+      To help match you with the right type of people, please answer a few more questions.
+    </p>
+    <p>
+      We promise it won't take long.
+    </p>
+  </div>
+`
+
+about = `
+  <div class="registrationContainer centerContainer" id="registrationContainerDiv">
+    <form name="registration">
+      <div class="form-group" style="position: relative" id="name">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="John"
+          name="name"
+          required
+        >
+      </div>
+
+      <div class="form-group" style="position: relative" id="email">
+        <input
+          type="email"
+          class="form-control"
+          placeholder="john@example.com"
+          name="email"
+          required
+        >
+      </div>
+
+      <div class="form-group" style="position: relative" id="password">
+        <input
+          type="password"
+          class="form-control"
+          placeholder="********"
+          name="password"
+          required
+        >
+      </div>
+
+      <div class="col-half">
+        <h5>Gender</h5>
+        <div class="gender-input-group">
+          <input
+            id="male"
+            type="radio"
+            name="gender"
+            value="male"
+          >
+          <label for="male">Male</label>
+          <input
+            id="female"
+            type="radio"
+            name="gender"
+            value="female"
+          >
+          <label for="female">Female</label>
+        </div>
+      </div>
+
+      <div>
+        <div>Date of Birth</div>
+        <div class="form-group col-md-4" style="padding-left: 0">
+          <select name="birthMonth" class="form-control" required>
+            <option>Month</option>
+            <option>January</option>
+            <option>February</option>
+            <option>March</option>
+            <option>April</option>
+            <option>May</option>
+            <option>June</option>
+            <option>July</option>
+            <option>August</option>
+            <option>September</option>
+            <option>October</option>
+            <option>November</option>
+            <option>December</option>
+          </select>
+        </div>
+        <div class="form-group col-md-4">
+          <select name="birthDate" class="form-control" required>
+            <option>Day</option>
+            ${day()}
+          </select>
+        </div>
+        <div class="form-group col-md-4">
+          <select name="birthYear" class="form-control" required>
+            <option>Year</option>
+            ${year()}
+          </select>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4 col-md-offset-4 text-center">
+          <button onclick="handleSignUp(event)" class="btn btn-success" name="signUpButton">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+`;
+
+const profileAboutPage = layout + almostDone + title + about
+
+window.addEventListener('load', () => {
+  if (window.location.pathname === '/register/about') {
+    document.getElementById('my-app').innerHTML = profileAboutPage;
+    Object.assign(document.getElementById('copy').style, titleStyle)
+  }
+})
