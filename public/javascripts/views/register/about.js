28323ac78e3de064db9a4afda701c665772a4022
fsultani <farid@Personal.local>
2018-07-04 19:45:37 -0700

    Add country/state/city to reg/about page

--- public/javascripts/views/register/about.js
+++ public/javascripts/views/register/about.js
@@ -19,39 +19,86 @@ title = `
 
 window.addEventListener('load', () => {
   if (window.location.pathname === '/register/about') {
-    let countryName = `
-      <div class="registrationContainer">
-        <form name="about">
+    let countryList = `
+      <div>
+        <form name="countryForm">
           <div class="form-group col-md-4 col-md-offset-4" style="padding-left: 0">
-            <select name="birthMonth" class="form-control" required>
-              <option>Country</option>
+            <select name="countryName" class="form-control" required>
+              <option selected disabled>Country</option>
+    `
+    let stateList = `
+      <div>
+        <form name="stateForm">
+          <div class="form-group col-md-4 col-md-offset-4" style="padding-left: 0">
+            <select name="stateName" class="form-control" required>
+              <option selected disabled>State/Region</option>
+            </select>
+          </div>
+        </form>
+      </div>
+    `
+    let cityList = `
+      <div>
+        <form name="cityForm">
+          <div class="form-group col-md-4 col-md-offset-4" style="padding-left: 0">
+            <select name="cityName" class="form-control" required>
+              <option selected disabled>City</option>
+            </select>
+          </div>
+        </form>
+      </div>
     `
 
-    Promise.all([
-      axios.get('/register/api/country-list'),
-      axios.get('/register/api/state-list')
-    ])
-    .then(([country, state]) => {
+
+    axios.get('/register/api/all-countries')
+    .then(country => {
       country.data.map(country => {
-        countryName += `<option>${country.name}</option>`
+        countryList += `<option value="${country.id}">${country.name}</option>`
       })
-      // console.log("state.data\n", state.data)
-      countryName += `
-        </select>
-            </div>
-            <div class="row">
-              <div class="col-md-4 col-md-offset-4 text-center">
-                <button onclick="handleSignUp(event)" class="btn btn-success" name="signUpButton">
-                  Sign Up
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
+      countryList += `
+        </select></div></form></div>
       `
-      const profileAboutPage = layout + almostDone + title + countryName
+      let profileAboutPage = layout + almostDone + title + countryList + stateList + cityList
       document.getElementById('my-app').innerHTML = profileAboutPage;
       Object.assign(document.getElementById('copy').style, titleStyle)
+
+      const countrySelected = document.forms.countryForm.elements.countryName
+      const stateSelected = document.forms.stateForm.elements.stateName
+      const citySelected = document.forms.cityForm.elements.cityName
+
+      stateSelected.style.display = 'none'
+
+      countrySelected.onchange = () => {
+        stateSelected.options.length = 1
+        citySelected.options.length = 1
+
+        if (countrySelected.value === '231') {
+          stateSelected.style.display = 'block'
+          axios.get('/register/api/state-list?country=231')
+          .then(state => {
+            state.data.map(state => {
+              stateSelected.options[stateSelected.options.length] = new Option(state.name, state.id)
+            })
+            stateSelected.onchange = () => {
+              citySelected.options.length = 1
+
+              axios.get(`/register/api/city-list?city=${stateSelected.value}`)
+              .then(city => {
+                city.data.map(city => {
+                  citySelected.options[citySelected.options.length] = new Option(city.name, city.id)
+                })
+              })
+            }
+          })
+        } else {
+          axios.get(`/register/api/state-list?country=${countrySelected.value}`)
+          .then(state => {
+            state.data.map(state => {
+              citySelected.options[citySelected.options.length] = new Option(state.name, state.id)
+            })
+          })
+        }
+      }
     })
   }
 })
