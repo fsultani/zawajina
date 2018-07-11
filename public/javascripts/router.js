709a5f506575f649df882b91ebedd0339c26b853
fsultani <farid@Personal.local>
2018-07-11 02:48:47 -0700

    Save work

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -1,14 +1,34 @@
 window.addEventListener('load', () => {
   if (window.location.pathname === '/register') {
-    console.log("I'm in the register page")
-    document.getElementById('my-app').innerHTML = `
-        <p>This is the register page</p>
-        <a href="/register/about">About page</a>
-    `
+    [
+      '/javascripts/views/layout.js',
+      '/javascripts/views/register/personal-info.js',
+      'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+      'https://unpkg.com/axios/dist/axios.min.js',
+      '/stylesheets/style.css',
+      '/stylesheets/registration.css',
+      'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+      'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css',
+    ].forEach(function(type) {
+      if (type.substring(type.length - 3) === '.js') {
+        const script = document.createElement('script')
+        script.src = type
+        script.async = false
+        document.head.appendChild(script)
+      } else {
+        const link = document.createElement('link')
+        link.rel = 'stylesheet'
+        link.href = type
+        document.head.appendChild(link)
+      }
+    })
   } else if (window.location.pathname === '/register/about') {
-    document.getElementById('my-app').innerHTML = `
-        <p>This is the about page</p>
-        <a href="/register">Go to register page</a>
-    `
+    console.log("about")
+    ['/javascripts/views/layout.js', '/javascripts/views/register/about-user.js'].forEach(function(src) {
+      let script = document.createElement('script')
+      script.src = src
+      script.async = false
+      document.head.appendChild(script)
+    })
   }
 })
