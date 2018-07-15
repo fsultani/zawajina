7aa0ebcafd8de5a5e7d67465db51cb068401d84b
fsultani <farid@Personal.local>
2018-07-14 18:37:02 -0700

    Enable webpack with HMR and watch option

--- server.js
+++ server.js
@@ -12,98 +12,33 @@ const flash = require('connect-flash');
 const mongo = require('mongodb')
 const mongoose = require('mongoose')
 
-if (process.env.NODE_ENV === 'mlab-dev') {
-  require('./db_credentials')
-  mongoose.connect(process.env.MONGO_DB_MLAB_DEV)
-  console.log("Using mlab:", process.env.NODE_ENV)
-} else if (process.env.NODE_ENV === 'local') {
-  require('./db_credentials')
-  mongoose.connect(process.env.LOCAL)
-  console.log("Using local db - mongodb://localhost/my_match_local_dev")
-} else {
-  mongoose.connect(process.env.HEROKU)
-  console.log("Heroku deployment")
-}
+const webpackDevServer = require('webpack-dev-server');
+const webpack = require('webpack');
+const config = require('./webpack.config.js');
 
 const app = express();
 
-const index = require('./routes/index');
-const registerRoute = require('./routes/registerRoute');
-const users = require('./routes/users');
-const conversation = require('./routes/conversation');
-const messages = require('./routes/messages');
+const options = {
+  contentBase: './dist',
+  hot: true,
+  host: 'localhost'
+};
 
-const Conversation = require('./models/conversation')
+webpackDevServer.addDevServerEntrypoints(config, options);
+const compiler = webpack(config);
+const server = new webpackDevServer(compiler, options);
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+const DIST_DIR = path.join(__dirname, "dist");
 
-// Body Parser Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+//Serving the files on the dist folder
+app.use(express.static(DIST_DIR));
 
-// Express Session
-// app.set('trust proxy', 1) // trust first proxy 
-app.use(session({
-  secret: "farid's secret",
-  resave: true,
-  saveUninitialized: true,
-  // cookie: { secure: true }
-}))
-
-// Passport init
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Express Validator
-// app.use(express.json())
-
-// Connect Flash
-app.use(flash());
-
-app.use(function (req, res, next) {
-  res.locals.success_message = req.flash('success_message');
-  res.locals.error_message = req.flash('error_message');
-  res.locals.error = req.flash('error');
-  res.locals.logged_out_message = req.flash('logged_out_message');
-  res.locals.logged_in_user = req.user
-  next();
+//Send index.html when the user access the web
+app.get("*", function (req, res) {
+  res.sendFile(path.join(DIST_DIR, "index.html"));
 });
 
-// Catch all 'get' requests, and respond with public/index.html
-app.get('*', (req, res, next) => {
-  if (req.url.indexOf('/api/') === -1) {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-    // res.sendFile(path.join(__dirname, 'public/index-no-network.html'))
-  } else {
-    return next();
-  }
-})
-
-// Use index.js for any routes beginning with '/'
-app.use('/', index);
-app.use('/register', registerRoute);
-app.use('/users', users);
-app.use('/conversation', conversation);
-app.use('/messages', messages);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-});
+// app.listen(PORT);
 
 const port = process.env.PORT || 3000;
 
@@ -114,4 +49,104 @@ app.listen(port, function() {
   }
 })
 
-module.exports = app;
+// if (process.env.NODE_ENV === 'mlab-dev') {
+//   require('./db_credentials')
+//   mongoose.connect(process.env.MONGO_DB_MLAB_DEV)
+//   console.log("Using mlab:", process.env.NODE_ENV)
+// } else if (process.env.NODE_ENV === 'local') {
+//   require('./db_credentials')
+//   mongoose.connect(process.env.LOCAL)
+//   console.log("Using local db - mongodb://localhost/my_match_local_dev")
+// } else {
+//   mongoose.connect(process.env.HEROKU)
+//   console.log("Heroku deployment")
+// }
+
+// const index = require('./routes/index');
+// const registerRoute = require('./routes/registerRoute');
+// const users = require('./routes/users');
+// const conversation = require('./routes/conversation');
+// const messages = require('./routes/messages');
+
+// const Conversation = require('./models/conversation')
+
+// // Set static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // Body Parser Middleware
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cookieParser());
+
+// // Express Session
+// // app.set('trust proxy', 1) // trust first proxy 
+// app.use(session({
+//   secret: "farid's secret",
+//   resave: true,
+//   saveUninitialized: true,
+//   // cookie: { secure: true }
+// }))
+
+// // Passport init
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// // Express Validator
+// // app.use(express.json())
+
+// // Connect Flash
+// app.use(flash());
+
+// app.use(function (req, res, next) {
+//   res.locals.success_message = req.flash('success_message');
+//   res.locals.error_message = req.flash('error_message');
+//   res.locals.error = req.flash('error');
+//   res.locals.logged_out_message = req.flash('logged_out_message');
+//   res.locals.logged_in_user = req.user
+//   next();
+// });
+
+// // Catch all 'get' requests, and respond with public/index.html
+// app.get('*', (req, res, next) => {
+//   if (req.url.indexOf('/api/') === -1) {
+//     res.sendFile(path.join(__dirname, 'public/index.html'))
+//     // res.sendFile(path.join(__dirname, 'public/index-no-network.html'))
+//   } else {
+//     return next();
+//   }
+// })
+
+// // Use index.js for any routes beginning with '/'
+// app.use('/', index);
+// app.use('/register', registerRoute);
+// app.use('/users', users);
+// app.use('/conversation', conversation);
+// app.use('/messages', messages);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   const err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+// });
+
+// const port = process.env.PORT || 3000;
+
+// app.listen(port, function() {
+//   console.log("Listening on port " + port)
+//   if (process.send) {
+//     process.send({ event:'online', url:'http://localhost:' + port})
+//   }
+// })
+
+// module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000
+  },
+  watch: true,
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js']
+  }
+};
