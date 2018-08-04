ffdf127e75bd5e813f719dfa3183554bcc03cf6a
fsultani <farid@Personal.local>
2018-08-04 13:21:13 -0700

    Test

--- public/javascripts/views/home.js
+++ public/javascripts/views/home.js
@@ -31,6 +31,7 @@ window.addEventListener('load', () => {
     const welcome = `
       <center>
         <h1>Welcome!</h1>
+        <h1>This is a test</h1>
         <h4>Please log in or create a free account</h4>
       </center>
     `;
