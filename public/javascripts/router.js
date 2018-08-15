e7560c0bebef97d0d2605c75d09b96445f0fdacc
fsultani <farid@Personal.local>
2018-08-15 00:38:09 -0700

    Update router

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -1,19 +1,19 @@
 import { layout } from './views/layout.js';
 import { register } from './views/register/1-personal.js';
-import { bootstrapMinCss, bootstrapThemeMinCss, bootstrapJs } from './styles/bootstrap.js';
+import * as bootstrap from './styles/bootstrap.js';
 
 window.onload = () => {
   if (window.location.pathname === '/') {
     window.location.pathname = '/home'
   } else if (window.location.pathname === '/home') {
-    document.getElementsByTagName('head')[0].appendChild(bootstrapMinCss())
-    document.getElementsByTagName('head')[0].appendChild(bootstrapThemeMinCss())
-    document.getElementsByTagName('head')[0].appendChild(bootstrapJs())
+    document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapMinCss())
+    document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapThemeMinCss())
+    document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapJs())
     document.getElementById('my-app').innerHTML = layout;
   } else if (window.location.pathname === '/register') {
-    document.getElementsByTagName('head')[0].appendChild(bootstrapMinCss())
-    document.getElementsByTagName('head')[0].appendChild(bootstrapThemeMinCss())
-    document.getElementsByTagName('head')[0].appendChild(bootstrapJs())
+    document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapMinCss())
+    document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapThemeMinCss())
+    document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapJs())
     document.getElementById('my-app').innerHTML = register;
   }
 }
