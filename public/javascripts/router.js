8614f21a548b26225d85f29b043f690bd0278bed
fsultani <farid@Personal.local>
2018-08-15 00:44:06 -0700

    Clean up router

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -2,18 +2,20 @@ import { layout } from './views/layout.js';
 import { register } from './views/register/1-personal.js';
 import * as bootstrap from './styles/bootstrap.js';
 
+const addBootstrap = () => {
+  document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapMinCss())
+  document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapThemeMinCss())
+  document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapJs())
+}
+
 window.onload = () => {
   if (window.location.pathname === '/') {
     window.location.pathname = '/home'
   } else if (window.location.pathname === '/home') {
-    document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapMinCss())
-    document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapThemeMinCss())
-    document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapJs())
+    addBootstrap()
     document.getElementById('my-app').innerHTML = layout;
   } else if (window.location.pathname === '/register') {
-    document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapMinCss())
-    document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapThemeMinCss())
-    document.getElementsByTagName('head')[0].appendChild(bootstrap.bootstrapJs())
+    addBootstrap()
     document.getElementById('my-app').innerHTML = register;
   }
 }
