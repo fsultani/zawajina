48e4afeb2e86a0eb6279f9128e2a11fe44b5b61a
fsultani <farid@Personal.local>
2018-08-15 00:38:09 -0700

    Update router

--- public/javascripts/styles/bootstrap.js
+++ public/javascripts/styles/bootstrap.js
@@ -1,11 +1,11 @@
-const bootstrapMinCss = () => {
+export const bootstrapMinCss = () => {
   const bootstrapMinCssTag = document.createElement('link')
   bootstrapMinCssTag.rel = 'stylesheet'
   bootstrapMinCssTag.href = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'
   return bootstrapMinCssTag
 }
 
-const bootstrapThemeMinCss = () => {
+export const bootstrapThemeMinCss = () => {
   const bootstrapThemeMinCssTag = document.createElement('link')
   bootstrapThemeMinCssTag.rel = 'stylesheet'
   bootstrapThemeMinCssTag.href = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css'
@@ -13,10 +13,8 @@ const bootstrapThemeMinCss = () => {
 }
 
 
-const bootstrapJs = () => {
+export const bootstrapJs = () => {
   const bootstrapJsTag = document.createElement('script')
   bootstrapJsTag.src = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'
   return bootstrapJsTag
 }
-
-export { bootstrapMinCss, bootstrapThemeMinCss, bootstrapJs };
