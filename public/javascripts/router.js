f56a5dfcae661a46c83d7e1fbaf2d5c90afb47ca
fsultani <farid@Personal.local>
2018-09-09 02:48:18 -0700

    Add error handling to reg personal page

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -27,6 +27,53 @@ export default (() => {
     registrationFormElement.password.addEventListener("blur", handlePasswordErrorOnBlur)
     registrationFormElement.password.addEventListener("keyup", handlePasswordErrorOnKeyUp)
     registrationFormElement.password.addEventListener("keyup", isPasswordValid)
+
+    let nameEmailPassword = document.querySelectorAll("[name='name'], [name='email'], [name='password']")
+    let genderButtons = document.querySelectorAll("input[type='radio']")
+    let DOB = document.querySelectorAll("[name='birthMonth'], [name='birthDate'], [name='birthYear']")
+
+    for (let i = 0; i < nameEmailPassword.length; i++) {
+      nameEmailPassword[i].onkeyup = () => {
+        if (
+          registrationFormElement.name.checkValidity() &&
+          registrationFormElement.email.checkValidity() &&
+          isPasswordValid()
+        ) {
+          if (
+            registrationFormElement.male.checked ||
+            registrationFormElement.female.checked
+          ) {
+            registrationFormElement.signUpButton.disabled = false
+          } else {
+            for (let i = 0; i < genderButtons.length; i++) {
+              genderButtons[i].onchange = () => {
+                if (
+                  registrationFormElement.male.checked ||
+                  registrationFormElement.female.checked
+                ) {
+                  for (let i = 0; i < DOB.length; i++) {
+                    DOB[i].onchange = () => {
+                      if (
+                        registrationFormElement.birthMonth.value !== 'Month' &&
+                        registrationFormElement.birthDate.value !== 'Day' &&
+                        registrationFormElement.birthYear.value !== 'Year'
+                      ) {
+                        registrationFormElement.signUpButton.disabled = false
+                      } else {
+                        registrationFormElement.signUpButton.disabled = true
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        } else {
+          registrationFormElement.signUpButton.disabled = true
+        }
+      }
+    }
+
   } else if (window.location.pathname === '/register/about') {
     document.getElementById('app').innerHTML = layout + profileAbout;
   }
