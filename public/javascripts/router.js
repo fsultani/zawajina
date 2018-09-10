f588056e4b26bd3230633ca338648a4cfe705f93
fsultani <farid@Personal.local>
2018-09-09 20:50:27 -0700

    Abstract personal info validations

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -1,12 +1,6 @@
 import { layout } from './views/layout.js';
-import {
-  handleNameError,
-  handleEmailError,
-  handlePasswordErrorOnBlur,
-  handlePasswordErrorOnKeyUp,
-  isPasswordValid,
-  personalInfo
-} from '/javascripts/views/register/personalInfo.js';
+import { personalInfo } from '/javascripts/views/register/personalInfo.js';
+import PersonalInfoValidation from '/javascripts/views/register/personalInfoValidation.js';
 import profileAbout from '/javascripts/views/register/profileAbout.js';
 import WelcomeHomePage from './views/home.js';
 import loginPage from './views/login.js';
@@ -20,59 +14,7 @@ export default (() => {
     document.getElementById('app').innerHTML = layout + loginPage;
   } else if (window.location.pathname === '/register') {
     document.getElementById('app').innerHTML = layout + personalInfo;
-    const registrationFormElement = document.forms.registration.elements
-    registrationFormElement.signUpButton.disabled = true
-    registrationFormElement.name.addEventListener("blur", handleNameError)
-    registrationFormElement.email.addEventListener("blur", handleEmailError)
-    registrationFormElement.password.addEventListener("blur", handlePasswordErrorOnBlur)
-    registrationFormElement.password.addEventListener("keyup", handlePasswordErrorOnKeyUp)
-    registrationFormElement.password.addEventListener("keyup", isPasswordValid)
-
-    let nameEmailPassword = document.querySelectorAll("[name='name'], [name='email'], [name='password']")
-    let genderButtons = document.querySelectorAll("input[type='radio']")
-    let DOB = document.querySelectorAll("[name='birthMonth'], [name='birthDate'], [name='birthYear']")
-
-    for (let i = 0; i < nameEmailPassword.length; i++) {
-      nameEmailPassword[i].onkeyup = () => {
-        if (
-          registrationFormElement.name.checkValidity() &&
-          registrationFormElement.email.checkValidity() &&
-          isPasswordValid()
-        ) {
-          if (
-            registrationFormElement.male.checked ||
-            registrationFormElement.female.checked
-          ) {
-            registrationFormElement.signUpButton.disabled = false
-          } else {
-            for (let i = 0; i < genderButtons.length; i++) {
-              genderButtons[i].onchange = () => {
-                if (
-                  registrationFormElement.male.checked ||
-                  registrationFormElement.female.checked
-                ) {
-                  for (let i = 0; i < DOB.length; i++) {
-                    DOB[i].onchange = () => {
-                      if (
-                        registrationFormElement.birthMonth.value !== 'Month' &&
-                        registrationFormElement.birthDate.value !== 'Day' &&
-                        registrationFormElement.birthYear.value !== 'Year'
-                      ) {
-                        registrationFormElement.signUpButton.disabled = false
-                      } else {
-                        registrationFormElement.signUpButton.disabled = true
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        } else {
-          registrationFormElement.signUpButton.disabled = true
-        }
-      }
-    }
+    PersonalInfoValidation()
   } else if (window.location.pathname === '/register/about') {
     document.getElementById('app').innerHTML = layout + profileAbout;
   }
