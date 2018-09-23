692841ff31e27b8146189ffdf9d9036d3dcad415
fsultani <farid@Personal.local>
2018-09-23 08:50:32 -0700

    Save work

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -9,27 +9,25 @@ import loginPage from './views/login.js';
 export default (() => {
   if (!window.location.hash) {
     console.log("Empty hash")
-    window.location.hash = '#home'
+    window.location.hash = 'home'
   }
 
   window.addEventListener('popstate', (e) => {
-    console.log('e\n', e)
+    // console.log('e\n', e)
   })
 
   window.addEventListener('hashchange', () => {
     console.log('hash\n', window.location.hash)
     switch (window.location.hash) {
       case '#home':
+        // window.location.hash = ''
         document.getElementById('app').innerHTML = layout + WelcomeHomePage();
-        window.history.pushState({page: 'homePage'}, '', 'home');
-        window.history.pushState({page: 'homePage'}, '', 'home');
-        window.location.hash = ''
+        // window.history.pushState({page: 'homePage'}, '', 'home');
         break;
       case '#login':
+        // window.location.hash = ''
         document.getElementById('app').innerHTML = layout + loginPage;
-        window.history.pushState({page: 'loginPage'}, '', 'login');
-        window.history.pushState({page: 'loginPage'}, '', 'login');
-        window.location.hash = ''
+        // window.history.pushState({page: 'loginPage'}, '', 'login');
         break;
       default:
         break;
