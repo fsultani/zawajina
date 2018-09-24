949a914729a85ab44a1e2b4330f11fe205d44ffb
fsultani <farid@Personal.local>
2018-09-24 07:20:20 -0700

    Remove commented code

--- public/javascripts/views/register/about.js
+++ public/javascripts/views/register/about.js
@@ -160,31 +160,9 @@ const handleDone = event => {
         window.location.pathname = '/login'
       }, 3000)
     }
-      // const error = document.createElement('div')
-      // error.setAttribute('id', 'registrationError')
-      // error.classList.add("alert")
-      // error.classList.add("alert-danger")
-      // error.innerHTML = 'Email already exists'
-      // error.style.width = '100%';
-      // error.style.height = 'auto';
-      // error.style.textAlign = 'center';
-      // const container = document.getElementById('my-app')
-      // container.before(error)
   })
   .catch(error => {
     console.log("error\n", error)
-    // const errors = document.createElement('div')
-    // errors.setAttribute('id', 'errors')
-    // const errorMessagesArray = error.response.data.error.map(err => {
-    //   return `<p>${err.msg}</p>`
-    // })
-    // errors.innerHTML = errorMessagesArray.join('')
-    // errors.style.color = 'red';
-    // errors.style.width = '100%';
-    // errors.style.height = 'auto';
-    // errors.style.textAlign = 'center';
-    // const container = document.getElementById('registrationContainerDiv').parentNode
-    // container.insertBefore(errors, document.getElementById('registrationContainerDiv'))
   })
 }
 
