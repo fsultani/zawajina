69e67a5c4de478b3ea4db420cfffe49e366ff110
fsultani <farid@Farids-MBP.local>
2019-01-06 13:27:50 -0800

    Save work

--- public/javascripts/views/login.js
+++ public/javascripts/views/login.js
@@ -1,15 +1,16 @@
-export default `
-  <div align="center">
-    <h2 class="page-header">Account Login</h2>
-    <form name="loginForm">
-    <form>
-      <div class="form-group">
-        <input type="text" class="form-control" name="email" placeholder="Email" style="width: 20%" required>
-      </div>
-      <div class="form-group">
-        <input type="password" class="form-control" name="password" placeholder="Password" style="width: 20%" required>
-      </div>
-      <input type="button" class="btn btn-success" onclick="login()" value="Submit" />
-    </form>
-  </div>
-`
+export default (() => `
+    <div align="center">
+      <h2 class="page-header">Account Login</h2>
+      <form name="loginForm">
+      <form>
+        <div class="form-group">
+          <input type="text" class="form-control" name="email" placeholder="Email" style="width: 20%" required>
+        </div>
+        <div class="form-group">
+          <input type="password" class="form-control" name="password" placeholder="Password" style="width: 20%" required>
+        </div>
+        <button class="btn btn-success" id="login">Login</button>
+      </form>
+    </div>
+  `
+)
