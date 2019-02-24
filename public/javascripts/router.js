e1c10d6874d533d1f497606ff9daa388c22220b2
Farid Sultani <faridsultani.ba@gmail.com>
2019-02-24 07:37:47 -0800

    Change WelcomeHome to welcome

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -3,7 +3,7 @@ import { personalInfo } from './views/register/personalInfo/index.js';
 import PersonalInfoValidation from './views/register/personalInfo/validations.js';
 import HandleSignUp from './views/register/personalInfo/handleSignUp.js';
 import profileAbout from './views/register/about/index.js';
-import WelcomeHomePage from './views/home.js';
+import welcome from './views/welcome.js';
 import loginPage from './views/login.js';
 
 const Router = () => {
@@ -18,7 +18,7 @@ const Router = () => {
   logoutScript.src = 'javascripts/scripts/Logout.js';
   document.head.appendChild(logoutScript);
   if (Cookies.get('token')) {
-    layout() + WelcomeHomePage();
+    layout() + welcome();
   } else {
     window.history.pushState(null, null, '/login');
     document.getElementById('app').innerHTML = layout() + loginPage();
@@ -27,18 +27,18 @@ const Router = () => {
   window.addEventListener('popstate', (e) => {
     switch (window.location.pathname) {
       case '/':
-        layout() + WelcomeHomePage();
-        // document.getElementById('app').innerHTML = layout() + WelcomeHomePage();
+        layout() + welcome();
+        // document.getElementById('app').innerHTML = layout() + welcome();
         break;
       case '/login':
-        // layout() + WelcomeHomePage();
+        // layout() + welcome();
         // document.getElementById('app').innerHTML = layout() + loginPage();
         break;
       case '/register':
         // document.getElementById('app').innerHTML = layout() + personalInfo;
         break;
       case '/home':
-        // document.getElementById('app').innerHTML = layout() + WelcomeHomePage();
+        // document.getElementById('app').innerHTML = layout() + welcome();
         break;
       default:
         break;
@@ -49,7 +49,7 @@ const Router = () => {
     switch (window.location.hash) {
       case '#home':
         window.history.replaceState(null, null, '/');
-        layout() + WelcomeHomePage();
+        layout() + welcome();
         break;
       case '#login':
         window.history.replaceState(null, null, '/login');
