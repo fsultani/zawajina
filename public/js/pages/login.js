caad5ddabb7f3d5e82bf12a914182c4000558462
Farid Sultani <faridsultani.ba@gmail.com>
2019-03-31 18:47:06 -0700

    Abstract away loginPageCss

--- public/js/pages/login.js
+++ public/js/pages/login.js
@@ -1,4 +1,76 @@
-const login = () => document.getElementById('app').innerHTML = `
+const loginPageCss = () => {
+  let fontAwesomeCss;
+  let animateCss;
+  let hamburgersCss;
+  let select2Css;
+  let utilCss;
+  let mainCss;
+
+  const loginPageCssLinks = []
+  const loginPageCssHrefs = [
+    {
+      href: "/static/css/fonts/font-awesome-4.7.0/css/font-awesome.min.css"
+    },
+    {
+      href: "/static/css/animate.css"
+    },
+    {
+      href: "/static/css/hamburgers.min.css"
+    },
+    {
+      href: "/static/css/select2.min.css"
+    },
+    {
+      href: "/static/css/util.css"
+    },
+    {
+      href: "/static/css/login-main.css"
+    }
+  ]
+
+  loginPageCssLinks.push(
+    fontAwesomeCss,
+    animateCss,
+    hamburgersCss,
+    select2Css,
+    utilCss,
+    mainCss
+  )
+
+  loginPageCssLinks.map((element, index) => {
+    element = document.createElement('link')
+    element.rel = "stylesheet"
+    element.href = loginPageCssHrefs[index].href
+    document.head.appendChild(element)
+  });
+
+  const bootstrapCss = document.createElement('link')
+  bootstrapCss.src = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+  bootstrapCss.integrity = "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+  bootstrapCss.crossOrigin = "anonymous"
+
+  const popper = document.createElement('script')
+  popper.src = 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'
+  popper.integrity = "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+  popper.crossOrigin = "anonymous"
+
+  const jQuery = document.createElement('script')
+  jQuery.src = 'https://code.jquery.com/jquery-3.3.1.slim.min.js'
+  jQuery.integrity = "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+  jQuery.crossOrigin = "anonymous"
+
+  const bootstrapJs = document.createElement('script')
+  bootstrapJs.src = 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'
+  bootstrapJs.integrity = "sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" 
+  bootstrapJs.crossOrigin = "anonymous"
+
+  document.head.appendChild(bootstrapCss)
+  document.body.appendChild(jQuery)
+  document.body.appendChild(popper)
+  document.body.appendChild(bootstrapJs)
+}
+
+const Login = () => document.getElementById('app').innerHTML = `
   <div class="limiter">
     <div class="container-login100">
       <div class="wrap-login100">
@@ -54,4 +126,4 @@ const login = () => document.getElementById('app').innerHTML = `
   </div>
 `;
 
-export default login;
+export { loginPageCss, Login};
--- public/js/router.js
+++ public/js/router.js
@@ -4,80 +4,8 @@ import PersonalInfoValidation from './pages/register/personalInfo/validations.js
 import HandleSignUp from './pages/register/personalInfo/handleSignUp.js';
 import profileAbout from './pages/register/about/index.js';
 import welcome from './pages/welcome.js';
-import login from './pages/login.js';
+import { loginPageCss, Login } from './pages/login.js';
 import memberProfile from './pages/memberProfile.js';
-
-const loginPageCss = () => {
-  let fontAwesomeCss;
-  let animateCss;
-  let hamburgersCss;
-  let select2Css;
-  let utilCss;
-  let mainCss;
-
-  const loginPageCssLinks = []
-  const loginPageCssHrefs = [
-    {
-      href: "/static/css/fonts/font-awesome-4.7.0/css/font-awesome.min.css"
-    },
-    {
-      href: "/static/css/animate.css"
-    },
-    {
-      href: "/static/css/hamburgers.min.css"
-    },
-    {
-      href: "/static/css/select2.min.css"
-    },
-    {
-      href: "/static/css/util.css"
-    },
-    {
-      href: "/static/css/login-main.css"
-    }
-  ]
-
-  loginPageCssLinks.push(
-    fontAwesomeCss,
-    animateCss,
-    hamburgersCss,
-    select2Css,
-    utilCss,
-    mainCss
-  )
-
-  loginPageCssLinks.map((element, index) => {
-    element = document.createElement('link')
-    element.rel = "stylesheet"
-    element.href = loginPageCssHrefs[index].href
-    document.head.appendChild(element)
-  });
-
-  const bootstrapCss = document.createElement('link')
-  bootstrapCss.src = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
-  bootstrapCss.integrity = "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-  bootstrapCss.crossOrigin = "anonymous"
-
-  const popper = document.createElement('script')
-  popper.src = 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'
-  popper.integrity = "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-  popper.crossOrigin = "anonymous"
-
-  const jQuery = document.createElement('script')
-  jQuery.src = 'https://code.jquery.com/jquery-3.3.1.slim.min.js'
-  jQuery.integrity = "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-  jQuery.crossOrigin = "anonymous"
-
-  const bootstrapJs = document.createElement('script')
-  bootstrapJs.src = 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'
-  bootstrapJs.integrity = "sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" 
-  bootstrapJs.crossOrigin = "anonymous"
-
-  document.head.appendChild(bootstrapCss)
-  document.body.appendChild(jQuery)
-  document.body.appendChild(popper)
-  document.body.appendChild(bootstrapJs)
-}
 
 window.onload = () => {
   const doc = document
@@ -164,7 +92,7 @@ window.onload = () => {
       } else if (page === 'login') {
         loginPageCss();
         window.history.replaceState({ page: 'login'}, null, '/login');
-        login();
+        Login();
       } else if (page === 'register') {
         window.history.replaceState({ page: 'register'}, null, '/register');
         document.getElementById('app').innerHTML = layout() + personalInfo;
@@ -190,7 +118,7 @@ window.onload = () => {
     } else if (page === 'login') {
       loginPageCss();
       window.history.replaceState({ page: 'login'}, null, '/login');
-      login();
+      Login();
     } else if (page === 'register') {
       window.history.replaceState({ page: 'register'}, null, '/register');
       document.getElementById('app').innerHTML = layout() + personalInfo;
@@ -210,7 +138,7 @@ window.onload = () => {
     } else if (hash === '#login') {
       loginPageCss();
       window.history.replaceState({ page: 'login'}, null, '/login');
-      login();
+      Login();
     } else if (hash === '#register') {
       window.history.replaceState({ page: 'register'}, null, '/register');
       document.getElementById('app').innerHTML = layout() + personalInfo;
