9db36de403a5fce939b456088ba79f473dacdda7
Farid Sultani <faridsultani.ba@gmail.com>
2019-08-24 23:31:26 -0700

    Save

---
+++ mongo_shell_notes.js
@@ -0,0 +1,60 @@
+mongo ds139322.mlab.com:39322/my_match_dev -u farid -p farid
+
+// Return a count of all users with a given name
+db.users.find().count()
+db.users.find({ name: "John"}).count()
+db.users.find({ name: "Test"}).count()
+
+// Remove all users with a specific name
+db.users.remove({ name: "John"})
+db.users.remove({ name: "Test"})
+
+// Delete a collection
+db.messages.drop() && db.conversations.drop()
+db.conversations.find().pretty()
+
+// Update a user's document
+db.users.update({name: "Farid"}, { $set: { "messages": []}})
+
+// Udate all documents
+db.users.updateMany({}, { $set: { "messages": []}})
+db.users.updateMany({}, { $set: { "conversations": []}})
+db.users.update({name: {$regex: "^"}}, { $set: { "messages": []}}, {"multi": true})
+
+// Remove all usernames that end in a number
+db.users.remove({username: {$regex: "[0-9]$"}})
+
+
+// Prints each item on a separate line
+db.users.find().forEach(function(u) { print(u.name) })
+
+// Returns an array
+db.users.find().map( function(u) { return u.name } )
+
+// Find a single user by first name
+db.users.find({ name: "John" }).pretty()
+
+// Find all users
+db.users.find().pretty()
+
+// Find all documents based on a certain criteria
+Message.find({ from_user_id: {"$in": senders}}).exec((err, msg) => { console.log(msg)})
+
+// When pushing a local branch to master
+git checkout master
+git merge branch
+git push
+
+// When pushing to Heroku
+if (process.env.NODE_ENV === 'mlab-dev') {
+  require('./db_credentials')
+  mongoose.connect(process.env.MONGO_DB_MLAB_DEV)
+  console.log("Using mlab:", process.env.NODE_ENV)
+} else if (process.env.NODE_ENV === 'local') {
+  require('./db_credentials')
+  mongoose.connect(process.env.LOCAL)
+  console.log("Using local db - mongodb://localhost/my_match_local_dev")
+} else {
+  mongoose.connect(process.env.MONGO_DB)
+  console.log("Heroku deployment")
+}
---
+++ routes/registerRoute.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { check, body, validationResult } = require('express-validator/check');
+const countries = require('country-state-city');
+const jwt = require('jwt-simple');
+const JWT_SECRET = Buffer.from('fe1a1915a379f3be5394b64d14794932', 'hex');
+
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/api/personal-info', [
+  check('firstName').not().isEmpty().withMessage('Enter your first name'),
+  check('lastName').not().isEmpty().withMessage('Enter your last name'),
+  check('userEmail').isEmail().withMessage('Enter a valid email address'),
+  check('userPassword').not().isEmpty().withMessage('Enter a password'),
+  check('userPassword').isLength({ min: 8 }),
+], (req, res) => {
+  const { firstName, lastName, userEmail, userPassword } = req.body;
+  const getErrors = validationResult(req);
+
+  if (!getErrors.isEmpty()) {
+    return res.status(400).json({ error: getErrors.array() });
+  } else {
+    User.findOne({ userEmail }, (err, userExists) => {
+      if (!userExists) {
+        const newUser = new User ({
+          firstName,
+          lastName,
+          userEmail,
+          userPassword
+        });
+
+        User.createUser(newUser, (err, user) => {
+          const token = jwt.encode({ email: user.userEmail }, JWT_SECRET);
+          const userId = user._id;
+          return res.status(201).send({ token });
+        })
+      } else if (userExists.userEmail) {
+        return res.status(200).json({ error: "Email already exists"});
+      } else {
+        return res.json({ error: "Unknown error" });
+      }
+    })
+  }
+})
+
+module.exports = router;
