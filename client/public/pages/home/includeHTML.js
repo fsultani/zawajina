d63aca142dee0dc40f6bb171c6ec15c2b3f1ded3
Farid Sultani <faridsultani.ba@gmail.com>
2019-10-02 21:23:19 -0700

    Refactor code

--- client/public/pages/home/includeHTML.js
+++ client/public/pages/home/includeHTML.js
@@ -1,24 +1,18 @@
 !function includeHTML() {
-  let z, i, elmnt, file, xhttp;
-  
+  let allElements, i, element, file;
+
   /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    function getFile() {
-      return axios.get(file);
-    }
+  allElements = document.getElementsByTagName("*");
+
+  /* Return an array from the HTMLCollections object */
+  allElements = Array.from(allElements);
+  allElements.map(element => {
+    file = element.getAttribute("w3-include-html");
     if (file) {
-      elmnt.removeAttribute("w3-include-html");
-      getFile().then(res => {
-        if (res.status === 200) {
-          elmnt.innerHTML = res.data
-        }
-      })
-      includeHTML();
-      return;
+      element.removeAttribute("w3-include-html");
+      fetch(file)
+        .then(response => response.text())
+        .then(res => element.innerHTML = res);
     }
-  }
+  })
 }();
