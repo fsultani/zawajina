61a3c420180e8d224821c454cdc42a34fd39d0f0
Farid Sultani <faridsultani.ba@gmail.com>
2019-10-14 18:31:28 -0700

    Modify input fields

--- server/index.js
+++ server/index.js
@@ -65,7 +65,6 @@ app.get('*', (req, res, next) => {
       break;
     case '/signup':
       res.sendFile(path.join(__dirname, '../client/pages/signup/Step_1_Signup/index.html'));
-      // res.sendFile(path.join(__dirname, '../client/pages/signup/test_template/index.html'));
       break;
     default:
       // res.sendFile(path.join(__dirname, '../client/router.html'));
