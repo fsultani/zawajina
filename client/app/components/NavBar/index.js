840da410a9cca4e1c41765bac716c7fd989e970e
Farid Sultani <faridsultani.ba@gmail.com>
2020-02-09 21:05:07 -0800

    Save work with main page

---
+++ client/app/components/NavBar/index.js
@@ -0,0 +1,37 @@
+const NavBar = () => {
+  const googleapis = document.createElement('link');
+  googleapis.rel = "stylesheet";
+  googleapis.href = "https://fonts.googleapis.com/css?family=Lato:400,400i|PT+Serif:700";
+
+  const navBarCss = document.createElement('link');
+  navBarCss.rel = "stylesheet"
+  navBarCss.href = '/static/client/app/components/NavBar/navBarStyles.css';
+
+  document.head.appendChild(googleapis);
+  document.head.appendChild(navBarCss);
+
+  const navBar = `
+    <div class="navbar--wrapper">
+      <header class="navbar--site-header">
+        <div class="navbar--container">
+          <div class="navbar--site-header-inner">
+            <div>
+              <a href="/"><img src="/static/client/images/home.svg" alt="Home"></a>
+            </div>
+            <ul class="navbar--header-links margin-0">
+              <li>
+                <a href="/search">Search</a>
+              </li>
+              <li>
+                <a href="/logout">Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </div>
+  `;
+  document.getElementById('navbar').innerHTML = navBar;
+}
+
+export default NavBar;
--- client/app/router.js
+++ client/app/router.js
@@ -1,33 +1,3 @@
-// const navBarCss = document.createElement('link')
-// navBarCss.src = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+import App from './index.js';
 
-axios.get("/api/user-details").then(res => {
-  const { name } = res.data;
-  const navBar = `
-    <center>
-      <h1>Welcome home, ${name}!</h1>
-    </center>
-
-    <header class="navbar--site-header">
-      <div class="navbar--container">
-        <div class="navbar--site-header-inner">
-          <div>
-            <a href="/"><img src="/static/client/images/home.svg" alt="Home"></a>
-          </div>
-          <ul class="navbar--header-links list-reset margin-0">
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/login">Login</a>
-            </li>
-            <li>
-              <a class="button navbar--button-sm navbar--button-shadow" href="/signup">Signup</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </header>
-  `;
-  document.getElementById('app').innerHTML = navBar;
-})
+window.onload = () => App();
