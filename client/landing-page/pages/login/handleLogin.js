cb638cf77e02b7ba47f0834ec33e0577faa4217d
Farid Sultani <faridsultani.ba@gmail.com>
2020-02-22 22:44:07 -0800

    Save error state for login page

--- client/landing-page/pages/login/handleLogin.js
+++ client/landing-page/pages/login/handleLogin.js
@@ -1,14 +1,40 @@
+const handleEmailValidation = () => {
+  const email = document.login.elements.email.value;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+const handlePasswordValidation = () => {
+  const password = document.login.elements.password.value;
+  return password.length > 0;
+}
+
 const handleLogin = event => {
   event.preventDefault();
   const email = document.login.elements.email.value;
   const password = document.login.elements.password.value;
 
-  axios.post('/login', {
-    email,
-    password
-  }).then(res => {
-    Cookies.set('token', res.data.token)
-    Cookies.set('userId', res.data.member._id)
-    window.location.pathname = '/';
-  })
+  const emailIsValid = handleEmailValidation();
+  const passwordIsValid = handlePasswordValidation();
+
+  if (emailIsValid && passwordIsValid) {
+    axios.post('/login', {
+      email,
+      password
+    }).then(res => {
+      Cookies.set('token', res.data.token)
+      Cookies.set('userId', res.data.member._id)
+      window.location.pathname = '/';
+    })
+  } else {
+    if (!emailIsValid) {
+      document.login.email.blur()
+      document.getElementById('email').classList.add('email-error')
+    };
+
+    if (!passwordIsValid) {
+      document.login.password.blur()
+      document.getElementById('password').classList.add('password-error')
+    };
+  }
 }
