41b8f21f69f5171a8db26e4bd854e23ea360d93c
Farid Sultani <faridsultani.ba@gmail.com>
2020-06-17 23:24:11 -0700

    Add images from aws

--- server/auto-signup.js
+++ server/auto-signup.js
@@ -1,156 +1,211 @@
-// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 const names = require('./names');
 const countries = require('country-state-city');
 
 const numberOfUsers = 5;
 
-// (async () => {
-//   const browser = await puppeteer.launch({ headless: false });
-//   const allPages = await browser.pages();
-//   const page = allPages[0];
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const allPages = await browser.pages();
+  const page = allPages[0];
 
-//   await page.goto('http://localhost:3000/signup');
+  await page.goto('http://localhost:3000/signup');
 
-//   const createFaridsUser = async () => {
-//     await page.waitForSelector('#name_id');
-//     await page.focus('#name_id');
-//     await page.keyboard.type("Farid");
+  const createFaridsUser = async () => {
+    await page.waitForSelector('#name_id');
+    await page.focus('#name_id');
+    await page.keyboard.type("Farid");
 
-//     await page.focus('#email_id');
-//     await page.keyboard.type('farid@me.com');
+    await page.focus('#email_id');
+    await page.keyboard.type('farid@me.com');
 
-//     await page.focus('#password');
-//     await page.keyboard.type('asdfasdf');
+    await page.focus('#password');
+    await page.keyboard.type('asdfasdf');
 
-//     await page.click('button[name=signupButton]');
+    await page.click('button[name=signupButton]');
 
-//     await page.waitForSelector('#dob-month');
-//     await page.select('#dob-month', 'october');
-//     await page.select('#dob-day', '14');
-//     await page.select('#dob-year', '1983');
+    await page.waitForSelector('#dob-month');
+    await page.select('#dob-month', 'october');
+    await page.select('#dob-day', '14');
+    await page.select('#dob-year', '1983');
 
-//     await page.evaluate(() => document.querySelector('input[type="radio"][id="male"]').click());
+    await page.evaluate(() => document.querySelector('input[type="radio"][id="male"]').click());
 
-//     const countriesList = countries.default.getAllCountries();
-//     const country = countriesList[Math.floor(Math.random() * countriesList.length)];
+    const countriesList = countries.default.getAllCountries();
+    const country = countriesList[Math.floor(Math.random() * countriesList.length)];
 
-//     await page.waitForSelector('#countries-list');
-//     await page.select('#countries-list', 'United States');
-//     await page.waitForSelector('#states-list');
-//     await page.select('#states-list', 'California');
-//     await page.waitForSelector('#cities-list');
-//     await page.select('#cities-list', 'Orange');
+    await page.waitForSelector('#countries-list');
+    await page.select('#countries-list', 'United States');
+    await page.waitForSelector('#states-list');
+    await page.select('#states-list', 'California');
+    await page.waitForSelector('#cities-list');
+    await page.select('#cities-list', 'Orange');
 
-//     await page.waitForSelector('.signup-button');
-//     await page.click('button[name=createNewAccount]');
+    await page.waitForSelector('.signup-button');
+    await page.click('button[name=createNewAccount]');
 
-//     await page.waitForSelector('#logout');
-//     await page.evaluate(() => document.querySelector('#logout').click());
-//   }
+    await page.waitForSelector('#logout');
+    await page.evaluate(() => document.querySelector('#logout').click());
+  }
 
-//   const createOtherUsers = async () => {
-//     const DeleteEntry = async () => {
-//       await page.keyboard.press('Home');
-//       await page.keyboard.down('Shift');
-//       await page.keyboard.press('End');
-//       await page.keyboard.up('Shift');
-//       await page.keyboard.press('Backspace');
-//     }
+  const createOtherUsers = async () => {
+    const DeleteEntry = async () => {
+      await page.keyboard.press('Home');
+      await page.keyboard.down('Shift');
+      await page.keyboard.press('End');
+      await page.keyboard.up('Shift');
+      await page.keyboard.press('Backspace');
+    }
 
-//     for (let i = 0; i < numberOfUsers; i++) {
-//       const coinToss = Math.round(Math.random());
-//       let nameChoice;
-//       // if (coinToss === 0) {
-//       //   nameChoice = {
-//       //     name: names.male[Math.floor(Math.random() * names.male.length)],
-//       //     gender: 'male'
-//       //   };
-//       // } else {
-//       //   nameChoice = {
-//       //     name: names.female[Math.floor(Math.random() * names.female.length)],
-//       //     gender: 'female'
-//       //   }
-//       // }
-//       nameChoice = {
-//         name: names.female[Math.floor(Math.random() * names.female.length)],
-//         gender: 'female'
-//       }
+    for (let i = 0; i < numberOfUsers; i++) {
+      const coinToss = Math.round(Math.random());
+      let nameChoice;
+      // if (coinToss === 0) {
+      //   nameChoice = {
+      //     name: names.male[Math.floor(Math.random() * names.male.length)],
+      //     gender: 'male'
+      //   };
+      // } else {
+      //   nameChoice = {
+      //     name: names.female[Math.floor(Math.random() * names.female.length)],
+      //     gender: 'female'
+      //   }
+      // }
+      nameChoice = {
+        name: names.female[Math.floor(Math.random() * names.female.length)],
+        gender: 'female'
+      }
 
-//       const birthMonths = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
-//       const birthMonth = birthMonths[Math.floor(Math.random() * birthMonths.length)];
-//       const birthDay = Math.floor(Math.random() * 31).toString();
-//       const birthYear = (1999 - (Math.floor(Math.random() * 30))).toString();
+      const birthMonths = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+      const birthMonth = birthMonths[Math.floor(Math.random() * birthMonths.length)];
+      const birthDay = Math.floor(Math.random() * 31).toString();
+      const birthYear = (1999 - (Math.floor(Math.random() * 30))).toString();
 
-//       await page.goto('http://localhost:3000/signup');
+      await page.goto('http://localhost:3000/signup');
 
-//       await page.waitForSelector('#name_id');
-//       await page.focus('#name_id');
-//       await page.keyboard.type(nameChoice.name);
+      await page.waitForSelector('#name_id');
+      await page.focus('#name_id');
+      await page.keyboard.type(nameChoice.name);
 
-//       await page.focus('#email_id');
-//       await page.keyboard.type(`${nameChoice.name.toLowerCase()}@me.com`);
+      await page.focus('#email_id');
+      await page.keyboard.type(`${nameChoice.name.toLowerCase()}@me.com`);
 
-//       await page.focus('#password');
-//       await page.keyboard.type('asdfasdf');
+      await page.focus('#password');
+      await page.keyboard.type('asdfasdf');
 
-//       await page.click('button[name=signupButton]');
+      await page.click('button[name=signupButton]');
 
-//       await page.on('response', async response => {
-//         if (response._status === 403) {
-//           console.log("response._status\n", response._status);
-//           await page.waitForSelector('#name_id');
-//           await page.focus('#name_id');
-//           DeleteEntry();
-//           await page.keyboard.type(nameChoice.name);
+      await page.on('response', async response => {
+        if (response._status === 403) {
+          console.log("response._status\n", response._status);
+          await page.waitForSelector('#name_id');
+          await page.focus('#name_id');
+          DeleteEntry();
+          await page.keyboard.type(nameChoice.name);
 
-//           await page.focus('#email_id');
-//           DeleteEntry();
-//           await page.keyboard.type(`${nameChoice.name.toLowerCase()}@me.com`);
+          await page.focus('#email_id');
+          DeleteEntry();
+          await page.keyboard.type(`${nameChoice.name.toLowerCase()}@me.com`);
 
-//           await page.click('button[name=signupButton]');
-//         }
-//       })
+          await page.click('button[name=signupButton]');
+        }
+      })
 
-//       await page.waitForSelector('#dob-month');
-//       await page.select('#dob-month', birthMonth);
-//       await page.select('#dob-day', birthDay);
-//       await page.select('#dob-year', birthYear);
+      await page.waitForSelector('#dob-month');
+      await page.select('#dob-month', birthMonth);
+      await page.select('#dob-day', birthDay);
+      await page.select('#dob-year', birthYear);
 
-//       nameChoice.gender === 'male' ?
-//       await page.evaluate(() => document.querySelector('input[type="radio"][id="male"]').click()) :
-//       await page.evaluate(() => document.querySelector('input[type="radio"][id="female"]').click());
+      nameChoice.gender === 'male' ?
+      await page.evaluate(() => document.querySelector('input[type="radio"][id="male"]').click()) :
+      await page.evaluate(() => document.querySelector('input[type="radio"][id="female"]').click());
 
-//       const countriesList = countries.default.getAllCountries();
-//       const country = countriesList[Math.floor(Math.random() * countriesList.length)];
+      const countriesList = countries.default.getAllCountries();
+      const country = countriesList[Math.floor(Math.random() * countriesList.length)];
 
-//       await page.select('#countries-list', country.name);
+      await page.select('#countries-list', country.name);
 
-//       if (country.id === 231) {
-//         const stateList = countries.default.getStatesOfCountry('231');
-//         const state = stateList[Math.floor(Math.random() * stateList.length)];
+      if (country.id === 231) {
+        const stateList = countries.default.getStatesOfCountry('231');
+        const state = stateList[Math.floor(Math.random() * stateList.length)];
 
-//         await page.waitForSelector('#states-list');
-//         await page.select('#states-list', state.name);
+        await page.waitForSelector('#states-list');
+        await page.select('#states-list', state.name);
 
-//         const citiesList = countries.default.getCitiesOfState(state.id);
-//         const city = citiesList[Math.floor(Math.random() * citiesList.length)];
-//         await page.select('#cities-list', city.name);
-//       } else {
-//         const citiesList = countries.default.getStatesOfCountry(country.id);
-//         const city = citiesList[Math.floor(Math.random() * citiesList.length)];
-//         await page.select('#cities-list', city.name);
-//       };
-//       await page.waitForSelector('.signup-button');
-//       await page.click('button[name=createNewAccount]');
+        const citiesList = countries.default.getCitiesOfState(state.id);
+        const city = citiesList[Math.floor(Math.random() * citiesList.length)];
+        await page.select('#cities-list', city.name);
+      } else {
+        const citiesList = countries.default.getStatesOfCountry(country.id);
+        const city = citiesList[Math.floor(Math.random() * citiesList.length)];
+        await page.select('#cities-list', city.name);
+      };
 
-//       await page.waitForSelector('#logout');
-//       await page.evaluate(() => document.querySelector('#logout').click());
-//     }
-//   }
+      await page.evaluate(() => {
+        window.scrollBy(0, window.innerHeight);
+      });
 
-//   // await createFaridsUser();
-//   await createOtherUsers();
+      const [fileChoose1] = await Promise.all([
+        page.waitForFileChooser(),
+        page.evaluate(() => document.querySelector('.image-1').click()),
+      ])
 
-//   console.log("done");
-//   await browser.close();
-// })();
+      await fileChoose1.accept(['/Users/farid/Downloads/trees/butterfly-1127666__480.jpg']);
+      await page.waitFor(500);
+
+      const [fileChoose2] = await Promise.all([
+        page.waitForFileChooser(),
+        page.evaluate(() => document.querySelector('.image-2').click()),
+      ])
+
+      await fileChoose2.accept(['/Users/farid/Downloads/trees/dandelion-445228__480.jpg']);
+      await page.waitFor(500);
+
+      const [fileChoose3] = await Promise.all([
+        page.waitForFileChooser(),
+        page.evaluate(() => document.querySelector('.image-3').click()),
+      ])
+
+      await fileChoose3.accept(['/Users/farid/Downloads/trees/forest-931706__480.jpg']);
+      await page.waitFor(500);
+
+      const [fileChoose4] = await Promise.all([
+        page.waitForFileChooser(),
+        page.evaluate(() => document.querySelector('.image-4').click()),
+      ])
+
+      await fileChoose4.accept(['/Users/farid/Downloads/trees/road-1072823__480.jpg']);
+      await page.waitFor(500);
+
+      const [fileChoose5] = await Promise.all([
+        page.waitForFileChooser(),
+        page.evaluate(() => document.querySelector('.image-5').click()),
+      ])
+
+      await fileChoose5.accept(['/Users/farid/Downloads/trees/the-road-815297__480.jpg']);
+      await page.waitFor(500);
+
+      const [fileChoose6] = await Promise.all([
+        page.waitForFileChooser(),
+        page.evaluate(() => document.querySelector('.image-6').click()),
+      ])
+
+      await fileChoose6.accept(['/Users/farid/Downloads/trees/tree-736885_1280.jpg']);
+      await page.waitFor(500);
+
+      await page.waitForSelector('.signup-button');
+      await page.click('button[name=createNewAccount]');
+      await page.waitFor(500);
+
+      await page.waitForSelector('#logout');
+      await page.evaluate(() => document.querySelector('#logout').click());
+      console.log("i: ", i);
+    }
+  }
+
+  // await createFaridsUser();
+  await createOtherUsers();
+
+  console.log("done");
+  await browser.close();
+})();
--- server/routes/registerRoute.js
+++ server/routes/registerRoute.js
@@ -1,3 +1,203 @@
+// const express = require('express');
+// const { check, body, validationResult } = require('express-validator/check');
+// const countries = require('country-state-city');
+// const jwt = require('jsonwebtoken');
+// const moment = require('moment')
+// const aws = require('aws-sdk');
+// const fs = require('fs')
+// const multer = require('multer');
+// const connectMultipart = require('connect-multiparty');
+// const imagemin = require('imagemin');
+// const imageminMozjpeg = require("imagemin-mozjpeg");
+// const imageminPngquant = require('imagemin-pngquant');
+
+// const s3Credentials = require('./s3Credentials.json');
+
+// const JWT_SECRET = Buffer.from('fe1a1915a379f3be5394b64d14794932', 'hex');
+
+// const User = require('../models/user');
+
+// const router = express.Router();
+
+// router.post('/api/personal-info', [
+//   check('name').not().isEmpty().withMessage('Enter your name'),
+//   check('email').isEmail().withMessage('Enter a valid email address'),
+//   check('password').not().isEmpty().withMessage('Enter a password'),
+//   check('password').isLength({ min: 8 }),
+// ], (req, res) => {
+//   const { name, email, password } = req.body;
+//   const getErrors = validationResult(req);
+
+//   if (!getErrors.isEmpty()) {
+//     return res.status(400).json({ error: getErrors.array() });
+//   } else {
+//     User.findOne({ email }, (err, userExists) => {
+//       // User does not exist; create a new account
+//       if (!userExists) {
+//         const newUser = new User ({
+//           name,
+//           email,
+//           password,
+//           startedRegistration: true,
+//           completedRegistration: false,
+//           isUserSessionValid: false,
+//         });
+
+//         User.createUser(newUser, (err, user) => {
+//           if (err) throw new Error(err);
+//           const userId = user._id;
+//           return res.status(201).send({ userId });
+//         })
+//       } else if (userExists.startedRegistration && !userExists.completedRegistration) {
+//         // User started registration, but did not complete it
+//         User.updateOne({ _id: userExists._id }, {
+//           $set: {
+//             name,
+//             password
+//           }
+//         }, (err, user) => {
+//           if (err) {
+//             return res.json({ error: "Unknown error" });
+//           } else {
+//             return res.status(200).json({ startedRegistration: userExists.startedRegistration });
+//           }
+//         })
+//       } else if (userExists.startedRegistration && userExists.completedRegistration) {
+//         return res.status(403).json({ error: "Account already exists" });
+//       } else {
+//         return res.json({ error: "Unknown error" });
+//       }
+//     })
+//   }
+// })
+
+// router.get('/api/all-countries', (req, res) => {
+//   const countryList = countries.default.getAllCountries();
+//   res.send(countryList);
+// })
+
+// router.get('/api/state-list', (req, res) => {
+//   const stateList = countries.default.getStatesOfCountry("231");
+//   res.send(stateList);
+// })
+
+// router.get('/api/cities-list', ({ query }, res) => {
+//   if (query.stateId) {
+//     res.send(countries.default.getCitiesOfState(query.stateId));
+//   } else {
+//     res.send(countries.default.getStatesOfCountry(query.countryId));
+//   }
+// })
+
+// const s3 = new aws.S3(s3Credentials);
+// const storage = multer.diskStorage({
+//   destination: 'uploads',
+//   filename: (req, file, cb) => {
+//     const random = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+//     const fileExtension = file.originalname.split('.')[1];
+//     cb(null, `${random}.${fileExtension}`);
+//   }
+// });
+
+// const upload = multer({ storage: storage });
+// router.post('/api/about', upload.fields([
+//   {
+//     name: 'image-1',
+//     maxCount: 1,
+//   },
+//   {
+//     name: 'image-2',
+//     maxCount: 1,
+//   },
+//   {
+//     name: 'image-3',
+//     maxCount: 1,
+//   },
+//   {
+//     name: 'image-4',
+//     maxCount: 1,
+//   },
+//   {
+//     name: 'image-5',
+//     maxCount: 1,
+//   },
+//   {
+//     name: 'image-6',
+//     maxCount: 1,
+//   },
+// ]), (req, res) => {
+//   const {
+//     birthMonth,
+//     birthDay,
+//     birthYear,
+//     gender,
+//     country,
+//     state,
+//     city,
+//   } = JSON.parse(req.body.userInfo);
+//   const dobMonth = moment().month(birthMonth).format('MM');
+//   const dobDate = moment().date(birthDay).format('DD');
+//   const fullDob = `${birthYear}${dobMonth}${dobDate}`;
+//   const age = moment().diff(fullDob, 'years');
+//   const userId = req.body.userId;
+
+//   Object.values(req.files).length > 0 && Object.values(req.files).map(async image => {
+//     const compressedFile = await imagemin(
+//       [image[0].path],
+//       {
+//         destination: 'compressed',
+//         plugins: [
+//           imageminMozjpeg({ quality: 50 }),
+//           imageminPngquant()
+//         ]
+//       }
+//     );
+
+//     s3.upload({
+//       Bucket: 'my-match',
+//       Key: `${userId}/${compressedFile[0].sourcePath.split('/')[1]}`,
+//       Body: compressedFile[0].data,
+//       ACL: 'public-read',
+//       ContentType: image[0].mimetype,
+//     }, (err, data) => {
+//       if (err) return console.log("err\n", err);
+//       fs.unlink(compressedFile[0].sourcePath, err => {
+//         if (err) return console.error(err);
+//         fs.unlink(compressedFile[0].destinationPath, err => {
+//           if (err) return console.error(err);
+//         })
+//       });
+
+//       User.findOneAndUpdate({ _id: userId }, {
+//         $push: { photos: data.Location }
+//       }, { new: true }, (err, user) => {
+//         if (err) return res.send({error: err})
+//       })
+//     })
+//   });
+
+//   User.findOneAndUpdate({ _id: userId }, {
+//     $set: {
+//       fullDob,
+//       age,
+//       gender,
+//       country,
+//       state,
+//       city,
+//       completedRegistration: true,
+//       isUserSessionValid: true,
+//     },
+//   }, { new: true }, (err, user) => {
+//     if (err) return res.send({error: err});
+//     const token = jwt.sign({ userDetails: user }, JWT_SECRET, { expiresIn: '1 day' });
+//     res.status(201).json({ token, userId });
+//   })
+// })
+
+// module.exports = router;
+
+/* *************************************************************************************************** */
+
 const express = require('express');
 const { check, body, validationResult } = require('express-validator/check');
 const countries = require('country-state-city');
