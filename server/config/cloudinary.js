c9fa46bd9ca0ab3f0131173a7210ee6e15b96c86
Farid Sultani <faridsultani.ba@gmail.com>
2022-04-26 08:43:04 -0700

    Refactor cloudinary credentials import logic

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,14 +1,13 @@
 const cloudinary = require('cloudinary').v2;
-const credentials = require('../credentials.json');
 
 module.exports = cloudinary.config({
   cloud_name: process.env.NODE_ENV === 'localhost'
-    ? credentials.CLOUDINARY_CLOUD_NAME
+    ? require('../credentials.json').CLOUDINARY_CLOUD_NAME
     : process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.NODE_ENV === 'localhost'
-    ? credentials.CLOUDINARY_API_KEY
+    ? require('../credentials.json').CLOUDINARY_API_KEY
     : process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.NODE_ENV === 'localhost'
-    ? credentials.CLOUDINARY_API_SECRET
+    ? require('../credentials.json').CLOUDINARY_API_SECRET
     : process.env.CLOUDINARY_API_SECRET,
 });
