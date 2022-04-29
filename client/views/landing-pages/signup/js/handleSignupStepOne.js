26e6f222518afa6c2784ed90f1d1d19f5af72a67
Farid Sultani <faridsultani.ba@gmail.com>
2022-04-29 05:32:35 -0700

    Add password protection to sign-up page

--- client/views/landing-pages/signup/js/handleSignupStepOne.js
+++ client/views/landing-pages/signup/js/handleSignupStepOne.js
@@ -56,63 +56,69 @@ const handlePasswordValidation = password => {
 };
 
 const handleSignupStepOne = async () => {
-  const nameValue = user_name.value;
-  const email = user_email.value;
-  const password = user_password.value;
+  const inviteCode = prompt("Enter your invite code");
 
-  handleNameValidation(nameValue);
-  handleEmailValidation(email);
-  handlePasswordValidation(password);
-
-  if (handleNameValidationValue && handleEmailValidationValue && handlePasswordValidationValue) {
-    loadingSpinner.style.display = 'inline-block';
-    signupButton.innerHTML = '';
-    signupButton.disabled = true;
-    signupButton.style.cursor = 'not-allowed';
-
-    document.querySelectorAll('form *').forEach(item => item.disabled = true);
-
-    const userIPAddress = await getUserIPAddress();
-
-    axios
-      .post('/register/api/personal-info', {
-        nameValue,
-        email,
-        password,
-        userIPAddress,
-      })
-      .then(res => {
-        Cookies.set('my_match_userId', res.data.userId, { sameSite: 'strict' });
-        // Cookies.set('emailVerificationToken', res.data.emailVerificationToken, { sameSite: 'strict' });
-        // if (res.status === 201) {
-        //   window.location.pathname = '/verify-email';
-        // } else if (res.status === 200) {
-        //   window.location.pathname = '/signup/profile';
-        // }
-        window.location.pathname = '/signup/profile';
-      })
-      .catch(error => {
-        console.error(error.response);
-        if (error.response.status === 403) {
-          /* Email address already exists */
-          loadingSpinner.style.display = 'none';
-          signupButton.innerHTML = 'Create Account';
-          signupButton.disabled = false;
-          signupButton.style.cursor = 'pointer';
-
-          document.getElementById('email-exists-error').innerHTML = error.response.data.error;
-          document.getElementById('email-exists-error').style.display = 'block';
-        } else {
-          /* Display generic error message */
-          loadingSpinner.style.display = 'none';
-          signupButton.innerHTML = 'Create Account';
-          signupButton.disabled = false;
-          signupButton.style.cursor = 'pointer';
-
-          document.getElementById('email-exists-error').innerHTML =
-            'We could not complete your request at this time.';
-          document.getElementById('email-exists-error').style.display = 'block';
-        }
-      });
+  if (inviteCode.toLowerCase() === 'mymatchinvite123') {
+    const nameValue = user_name.value;
+    const email = user_email.value;
+    const password = user_password.value;
+  
+    handleNameValidation(nameValue);
+    handleEmailValidation(email);
+    handlePasswordValidation(password);
+  
+    if (handleNameValidationValue && handleEmailValidationValue && handlePasswordValidationValue) {
+      loadingSpinner.style.display = 'inline-block';
+      signupButton.innerHTML = '';
+      signupButton.disabled = true;
+      signupButton.style.cursor = 'not-allowed';
+  
+      document.querySelectorAll('form *').forEach(item => item.disabled = true);
+  
+      const userIPAddress = await getUserIPAddress();
+  
+      axios
+        .post('/register/api/personal-info', {
+          nameValue,
+          email,
+          password,
+          userIPAddress,
+        })
+        .then(res => {
+          Cookies.set('my_match_userId', res.data.userId, { sameSite: 'strict' });
+          // Cookies.set('emailVerificationToken', res.data.emailVerificationToken, { sameSite: 'strict' });
+          // if (res.status === 201) {
+          //   window.location.pathname = '/verify-email';
+          // } else if (res.status === 200) {
+          //   window.location.pathname = '/signup/profile';
+          // }
+          window.location.pathname = '/signup/profile';
+        })
+        .catch(error => {
+          console.error(error.response);
+          if (error.response.status === 403) {
+            /* Email address already exists */
+            loadingSpinner.style.display = 'none';
+            signupButton.innerHTML = 'Create Account';
+            signupButton.disabled = false;
+            signupButton.style.cursor = 'pointer';
+  
+            document.getElementById('email-exists-error').innerHTML = error.response.data.error;
+            document.getElementById('email-exists-error').style.display = 'block';
+          } else {
+            /* Display generic error message */
+            loadingSpinner.style.display = 'none';
+            signupButton.innerHTML = 'Create Account';
+            signupButton.disabled = false;
+            signupButton.style.cursor = 'pointer';
+  
+            document.getElementById('email-exists-error').innerHTML =
+              'We could not complete your request at this time.';
+            document.getElementById('email-exists-error').style.display = 'block';
+          }
+        });
+    }
+  } else {
+    confirm('Invalid code')
   }
 };
