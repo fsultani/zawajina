ac2a6a002178052edc8e5b6cb67322c647edb13e
Farid Sultani <faridsultani.ba@gmail.com>
2022-10-23 15:06:00 +0300

    Make trivial change to update heroku deployment
    - heroku-22 will replace heroku-18

--- server/index.js
+++ server/index.js
@@ -384,7 +384,7 @@ if (process.env.NODE_ENV === 'development') {
     end tell
 
     `,
-    (err, result, raw) => {
+    (err, _result, _raw) => {
       if (err) return console.error(err);
     }
   );
