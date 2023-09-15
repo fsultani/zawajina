5ff1a599cca01ed35627142413a8cba7c65aeff2
Farid Sultani <faridsultani.ba@gmail.com>
2023-09-15 00:53:51 -0700

    Add more to the FAQ

--- client/views/landing-pages/faq/script.js
+++ client/views/landing-pages/faq/script.js
@@ -1,14 +1,23 @@
 const listOfFaq = [
   {
     question: `How is this site different from the rest?`,
-    answer: `It just is. We're much better. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    answer: `Most sites don't show mutual matches.&nbsp;&nbsp;Our algorithm takes into account what you are looking for, as well as the other person, and matches you based on your individual preferences.&nbsp;&nbsp;This saves you lots of time, and allows you to find meaningful and quality matches.`,
   },
   {
     question: `How much do you charge?`,
-    answer: `Free for now.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    <br>
-    <br>
-    Free for now.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    answer: `We are free for now.&nbsp;&nbsp;Enjoy!`,
+  },
+  {
+    question: `How do I search for users?`,
+    answer: `Just head to the search page from the navigation bar at the top.&nbsp;&nbsp;Set whatever search preferences you want, and click "Search".<br /><br />It's important to note that each preference you set works as a deal-breaker.&nbsp;&nbsp;For example, if you specify an age range, then you will only be shown users within that age range, and the users that are shown to you are interested in someone your age.`,
+  },
+  {
+    question: `How do I block someone?`,
+    answer: `You may block someone from their profile page.&nbsp;&nbsp;Click the three vertical dots next to their name, and choose "Block".&nbsp;&nbsp;You may also report users for inappropriate behavior.`,
+  },
+  {
+    question: `How do I delete my account?`,
+    answer: `You may delete your account from your settings page.`,
   },
 ];
 
