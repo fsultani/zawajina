a8333cced29b1543cafe38796cfccd822413f85e
Farid Sultani <faridsultani.ba@gmail.com>
2023-09-23 22:07:25 -0700

    Fix padding and margin on /signup/profile page
    - Revert handleCreateNewAccount() logic
    - Fix mobile padding on /verify-email page

--- server/helpers/languages.js
+++ server/helpers/languages.js
@@ -2,7 +2,6 @@ const languagesList = require('../data/languages');
 
 const languages = async (_req, res) => {
   const allLanguages = languagesList.default.getAllLanguages();
-  console.log(`allLanguages successfully loaded`);
   return res.status(200).json({ allLanguages });
 };
 
