94ad89de2cd1dcd7b81183db75d80c10522eebaf
Farid Sultani <faridsultani.ba@gmail.com>
2023-09-27 21:14:56 -0700

    Remove commented code

--- client/views/app/profile/js/sections/handleGoToConversation.js
+++ client/views/app/profile/js/sections/handleGoToConversation.js
@@ -1,14 +1,3 @@
-// const handleGoToConversation = userId => {
-//   axios
-//     .get(`/messages/api/conversation/user/${userId}`) // server/routes/messages.js
-//     .then(({ data }) => {
-//       window.location.pathname = data?.url;
-//     })
-//     .catch(error => {
-//       console.log(`error\n`, error);
-//     })
-// };
-
 const handleGoToConversation = userId => {
   Axios({
     apiUrl: `/messages/api/conversation/user/${userId}` // server/routes/messages.js
